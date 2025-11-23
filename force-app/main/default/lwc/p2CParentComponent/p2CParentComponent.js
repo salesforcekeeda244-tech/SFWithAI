f0cd10d1c09f7989deae51140e77a24942555566
Dollessh <dolleshrathod214@gmail.com>
2025-11-23 16:17:42 +0530

    Change to Dynamic Message

--- force-app/main/default/lwc/p2CParentComponent/p2CParentComponent.js
+++ force-app/main/default/lwc/p2CParentComponent/p2CParentComponent.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class P2CParentComponent extends LightningElement {}
+export default class P2CParentComponent extends LightningElement {
+    msg = 'Hello From Parent Dynamically!';
+}
